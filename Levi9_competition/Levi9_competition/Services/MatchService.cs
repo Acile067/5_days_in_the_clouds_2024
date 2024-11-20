58cd30c0519e215410c47aac8b384f19c5042e9e
Aleksandar <acile067@gmail.com>
2024-11-20 15:17:05 +0100

    bugfix

--- Levi9_competition/Levi9_competition/Services/MatchService.cs
+++ Levi9_competition/Levi9_competition/Services/MatchService.cs
@@ -1,6 +1,7 @@
 ﻿using Levi9_competition.Dtos.Match;
 using Levi9_competition.Interfaces;
 using Levi9_competition.Mappers;
+using Levi9_competition.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Levi9_competition.Services
@@ -24,17 +25,12 @@ namespace Levi9_competition.Services
                 throw new ArgumentException("Team1 and Team2 have same IDs.");
 
             if (string.IsNullOrEmpty(matchModel.Team1Id) || string.IsNullOrEmpty(matchModel.Team2Id))
-            {
                 throw new ArgumentException("Team1Id and Team2Id must be valid.");
-            }
 
             if (matchModel.WinningTeamId != null &&
                 matchModel.Team1Id != matchModel.WinningTeamId &&
                 matchModel.Team2Id != matchModel.WinningTeamId)
-            {
                 throw new ArgumentException("Invalid WinningTeamId.");
-            }
-
 
             var team1 = await _teamRepo.GetByIdAsync(matchModel.Team1Id);
             var team2 = await _teamRepo.GetByIdAsync(matchModel.Team2Id);
@@ -42,61 +38,23 @@ namespace Levi9_competition.Services
             if (team1 == null || team2 == null)
                 throw new ArgumentException("One or both teams do not exist.");
 
-            double team1AvgElo = team1.Players.Average(p => p.Elo);
-            double team2AvgElo = team2.Players.Average(p => p.Elo);
-            
-            double team1Score = matchModel.WinningTeamId == matchModel.Team1Id ? 1 :
-                                matchModel.WinningTeamId == matchModel.Team2Id ? 0 : 0.5;
-            double team2Score = 1 - team1Score;
+            double team1AvgElo = CalculateTeamELO(team1.Players);
+            double team2AvgElo = CalculateTeamELO(team2.Players);
 
-            foreach (var player in team1.Players)
+            bool isDraw = matchModel.WinningTeamId == null;
+
+            if (isDraw)
             {
-                int newHoursPlayed = player.HoursPlayed + matchModel.Duration;
-                int k = CalculateK(newHoursPlayed);
-                double expectedScore = CalculateExpectedScore(player.Elo, team2AvgElo);
-                
-                player.HoursPlayed = newHoursPlayed;
-                player.RatingAdjustment = k;
-
-                if (team1Score == 1) 
-                { 
-                    player.Wins++;
-                    player.Elo += Math.Abs((int)Math.Round(CalculateNewElo(player.Elo, expectedScore, team1Score, k)));
-                }
-                else if (team1Score == 0) 
-                { 
-                    player.Losses++;
-                    player.Elo += -Math.Abs((int)Math.Round(CalculateNewElo(player.Elo, expectedScore, team1Score, k)));
-                }
-                else
-                {
-                    player.Elo += Math.Abs((int)Math.Round(CalculateNewElo(player.Elo, expectedScore, team1Score, k)));
-                }
+                UpdateTeamPlayers(team1.Players, 0.5, matchModel.Duration, team2AvgElo);
+                UpdateTeamPlayers(team2.Players, 0.5, matchModel.Duration, team1AvgElo);
             }
-
-            foreach (var player in team2.Players)
+            else
             {
-                int newHoursPlayed = player.HoursPlayed + matchModel.Duration;
-                int k = CalculateK(newHoursPlayed);
-                double expectedScore = CalculateExpectedScore(player.Elo, team1AvgElo);
-                
-                player.HoursPlayed = newHoursPlayed;
-                player.RatingAdjustment = k;
+                double team1Score = matchModel.WinningTeamId == matchModel.Team1Id ? 1 : 0;
+                double team2Score = 1 - team1Score;
 
-                if (team2Score == 1) 
-                { 
-                    player.Wins++;
-                    player.Elo += Math.Abs((int)Math.Round(CalculateNewElo(player.Elo, expectedScore, team1Score, k)));
-                }
-                else if (team2Score == 0) 
-                { 
-                    player.Losses++;
-                    player.Elo += -Math.Abs((int)Math.Round(CalculateNewElo(player.Elo, expectedScore, team1Score, k)));
-                }
-                else
-                {
-                    player.Elo += Math.Abs((int)Math.Round(CalculateNewElo(player.Elo, expectedScore, team1Score, k)));
-                }
+                UpdateTeamPlayers(team1.Players, team1Score, matchModel.Duration, team2AvgElo);
+                UpdateTeamPlayers(team2.Players, team2Score, matchModel.Duration, team1AvgElo);
             }
 
             var match = matchModel.ToMatchFromCreateDTO();
@@ -106,6 +64,36 @@ namespace Levi9_competition.Services
             await _teamRepo.UpdateAsync(team2);
 
             return true;
+        }
+
+        private double CalculateTeamELO(IEnumerable<Player> players)
+        {
+            return players.Average(player => player.Elo);
+        }
+
+        private void UpdateTeamPlayers(IEnumerable<Player> players, double score, int duration, double opponentElo)
+        {
+            foreach (var player in players)
+            {
+                double expectedScore = CalculateExpectedScore(player.Elo, opponentElo);
+                int k = CalculateK(player.HoursPlayed);
+
+                player.Elo = (int)Math.Round(player.Elo + k * (score - expectedScore));
+
+                player.HoursPlayed += duration;
+
+                int k2 = CalculateK(player.HoursPlayed);
+                player.RatingAdjustment = k2;
+
+                if (score == 1)
+                {
+                    player.Wins++;
+                }
+                else if (score == 0)
+                {
+                    player.Losses++;
+                }
+            }
         }
         private int CalculateK(int hoursPlayed)
         {
@@ -118,11 +106,6 @@ namespace Levi9_competition.Services
         private double CalculateExpectedScore(double r1, double r2)
         {
             return 1 / (1 + Math.Pow(10, (r2 - r1) / 400));
-        }
-
-        private double CalculateNewElo(double r1, double expected, double actual, int k)
-        {
-            return r1 + k * (actual - expected);
         }
     }
 }
